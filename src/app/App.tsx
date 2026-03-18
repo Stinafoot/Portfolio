32277bfc7f3b2f9b7d1321445b14d877bce1c9d1
Stinafoot <stinafoot@yahoo.com>
2026-03-18 09:36:19 -0500

    Import Projects component in App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,7 +4,7 @@ import { Hero } from "./components/Hero";
 import { About } from "./components/About";
 import { Education } from "./components/Education";
 import { Skills } from "./components/Skills";
-// import { Projects } from "./components/Projects";
+import { Projects } from "./components/Projects";
 import { Certifications } from "./components/Certifications";
 import { Resume } from "./components/Resume";
 import { Contact } from "./components/Contact";
